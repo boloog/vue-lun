c59cc6c4f1f222bfb653aacbe85053b219a11ab2
龙柏龙 <lonbolong@outlook.com>
2019-06-05 14:37:21 +0800

    button组件添加svg icon图片，并用样式区别左右显示。

--- button.vue
+++ button.vue
@@ -1,29 +1,73 @@
 <template>
-    <button class="v-button">Hello~</button>
+    <button class="v-button" :class="{ [`icon-${iconPosition}`]: true }">
+        <svg v-if="icon" class="icon">
+            <use :xlink:href=`#icon-${icon}`></use>
+        </svg>
+        <div class="content">
+            <slot></slot>
+        </div>
+    </button>
 </template>
 
 <script>
     export default {
-
+        props: {
+            icon: {},
+            iconPosition: { // left right
+                type: String,
+                default: 'left',
+                validator(value){
+                    return value === 'left' || value === 'right'
+                }
+            }
+        }
+        // 两种参数写法
+        // props: ['icon', 'iconPosition']
     }
 </script>
 
 <style lang="scss">
-    .v-button{
+    .v-button {
         font-size: var(--font-size);
         height: var(--button-height);
         padding: 0 6px;
         border-radius: var(--border-radius);
         border: 1px solid var(--border-color);
         background: var(--button-background);
-        &:hover{
+        display: inline-flex;
+        justify-content: center;
+        align-items: center;
+        vertical-align: middle;
+        &:hover {
             border-color: var(--border-color-hover);
         }
-        &:active{
+
+        &:active {
             background-color: var(--button-active-background);
         }
-        &:focus{
+
+        &:focus {
             outline: none;
+        }
+
+        > .content {
+            order: 2;
+        }
+
+        > .icon {
+            order: 1; margin-right: 4px;
+        }
+
+        &.icon-right {
+            > .content {
+                order: 1;
+            }
+
+            > .icon {
+                order: 2;
+                margin-right: 0;
+                margin-left: 4px;
+            }
         }
     }
 </style>
